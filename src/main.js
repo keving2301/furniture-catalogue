8dce14de33ca504387e1d9adf5d71a2461e03975
Kevin García <kevingarcia2301@gmail.com>
2021-06-13 00:11:10 -0400

    Removing Firebase.js file not needed, tem initialization of firebase in main

--- src/main.js
+++ src/main.js
@@ -11,6 +11,19 @@ Vue.config.productionTip = false;
 Vue.component('app-alert', ErrorAlert)
 let app;
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBQ0pfKZAgX4jwTXfmh-7bBVWrc5OYAPTA",
+    authDomain: "furniture-catalogue-01.firebaseapp.com",
+    databaseURL: "https://furniture-catalogue-01-default-rtdb.firebaseio.com",
+    projectId: "furniture-catalogue-01",
+    storageBucket: "furniture-catalogue-01.appspot.com",
+    messagingSenderId: "545417372174",
+    appId: "1:545417372174:web:75602c93fb8577a76b925f",
+    measurementId: "G-P3G897RXHT"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 firebase.auth().onAuthStateChanged(user => {
     store.dispatch('auth/fetchUser', user).then();
     if (!app) {
